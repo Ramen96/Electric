6425eff07fc255f185df565c8627357ac10281fa
Ramen96 <eric.worsfold@gmail.com>
2025-04-08 23:26:16 -0400

    updated map

--- app/components/MapComponent/mapComponent.tsx
+++ app/components/MapComponent/mapComponent.tsx
@@ -7,7 +7,7 @@ export default function MapComponent() {
   }, []);
   return (
     <MapContainer 
-      center={[35.2271, -80.8431]} 
+      center={[35.2705877, -81.104609]} 
       zoom={11} 
       style={{ height: "100%", width: "100%" }}
     >
@@ -15,7 +15,7 @@ export default function MapComponent() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={[35.2271, -80.8431]}>
+      <Marker position={[35.2705877, -81.104609,]}>
         <Popup>
           Charlotte, NC<br />Our service area
         </Popup>
