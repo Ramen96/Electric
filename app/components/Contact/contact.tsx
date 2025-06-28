53b170cf7a4102308e373477757ba40dda6ca98b
Ramen96 <eric.worsfold@gmail.com>
2025-06-28 13:07:21 -0400

    phone input field now has auto formatting

--- app/components/Contact/contact.tsx
+++ app/components/Contact/contact.tsx
@@ -26,9 +26,30 @@ export default function Contact() {
   const [successMessage, setSuccessMessage] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
 
+  const formatPhoneNumber = (value) => {
+    const phoneNumber = value.replace(/\D/g, "");
+
+    if (phoneNumber.length < 4) {
+      return phoneNumber;
+    } else if (phoneNumber.length < 7) {
+      return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3)}`;
+    } else {
+      return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(
+        3,
+        6
+      )}-${phoneNumber.slice(6, 10)}`;
+    }
+  };
+
   const handleChange = (e) => {
     const { id, value } = e.target;
-    setFormState((prev) => ({ ...prev, [id]: value }));
+
+    if (id === "phone") {
+      const formattedPhone = formatPhoneNumber(value);
+      setFormState((prev) => ({ ...prev, [id]: formattedPhone }));
+    } else {
+      setFormState((prev) => ({ ...prev, [id]: value }));
+    }
   };
 
   const handleContactSubmit = async (formData: ContactFormData) => {
