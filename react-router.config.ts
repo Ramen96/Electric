c121f4b1c5e71487d41b0f866b04215c25dc8fa0
Ramen96 <eric.worsfold@gmail.com>
2025-07-01 21:52:48 -0400

    updated for build

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,4 +4,6 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
+  buildDirectory: "build",
+  basename: "/",
 } satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   server: {
-    host: true
+    host: true,
+    port: 3000
   },
 });
