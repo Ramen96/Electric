215fb4c8517ece402572ba167dfba584daeb17fb
Ramen96 <eric.worsfold@gmail.com>
2025-07-05 21:03:05 -0400

    removed unused code

--- app/components/MapComponent/mapComponent.tsx
+++ app/components/MapComponent/mapComponent.tsx
@@ -1,31 +1,3 @@
-// import { useEffect } from 'react';
-// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-
-// export default function MapComponent() {
-//   useEffect(() => {
-//     console.log(window.innerWidth);
-//   }, []);
-//   return (
-//     <MapContainer 
-//       center={[35.2705877, -81.104609]} 
-//       zoom={9} 
-//       style={{ height: "100%", width: "100%" }}
-//     >
-//       <TileLayer
-//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//       />
-//       <Marker position={[35.2705877, -81.104609,]}>
-//         <Popup>
-//           Charlotte, NC<br />Our service area
-//         </Popup>
-//       </Marker>
-//     </MapContainer>
-//   );
-// }
-
-
-
 import { useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
