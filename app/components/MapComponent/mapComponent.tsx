e76566d57d24c20f28a9e9e19a2a38a6448a7de0
Ramen96 <eric.worsfold@gmail.com>
2025-07-05 21:01:06 -0400

    fixed map icon

--- app/components/MapComponent/mapComponent.tsx
+++ app/components/MapComponent/mapComponent.tsx
@@ -1,10 +1,53 @@
+// import { useEffect } from 'react';
+// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
+// export default function MapComponent() {
+//   useEffect(() => {
+//     console.log(window.innerWidth);
+//   }, []);
+//   return (
+//     <MapContainer 
+//       center={[35.2705877, -81.104609]} 
+//       zoom={9} 
+//       style={{ height: "100%", width: "100%" }}
+//     >
+//       <TileLayer
+//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//       />
+//       <Marker position={[35.2705877, -81.104609,]}>
+//         <Popup>
+//           Charlotte, NC<br />Our service area
+//         </Popup>
+//       </Marker>
+//     </MapContainer>
+//   );
+// }
+
+
+
 import { useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+
+// Import marker icons
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+// Fix the default markers for production build
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerShadow,
+});
 
 export default function MapComponent() {
   useEffect(() => {
     console.log(window.innerWidth);
   }, []);
+  
   return (
     <MapContainer 
       center={[35.2705877, -81.104609]} 
@@ -15,7 +58,7 @@ export default function MapComponent() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={[35.2705877, -81.104609,]}>
+      <Marker position={[35.2705877, -81.104609]}>
         <Popup>
           Charlotte, NC<br />Our service area
         </Popup>
